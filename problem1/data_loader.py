7c5cf21b8c9f8f980c6f40277937daca4b86055e
nick <huang.xinyu@wanlitechnologies.com>
2020-08-06 11:56:03 +0800

    upload

--- problem1/data_loader.py
+++ problem1/data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
 pd.set_option('display.max_columns', 1000)
 import numpy as np
+import seaborn as sns
 import math
 import matplotlib.pyplot as plt
 import os
@@ -20,6 +21,10 @@ from sklearn.model_selection import cross_val_score
 cols = ['UpdateTime', 'UpdateMillisec', 'InstrumentID', 'Volume', 'LastPrice', 'OpenInterest', 'PreSettlementPrice',
         'PreClosePrice', 'PreOpenInterest', 'OpenPrice', 'HighestPrice', 'LowestPrice', 'ClosePrice', 'SettlementPrice',
         'UpperLimitPrice', 'LowerLimitPrice', 'BidPrice1', 'AskPrice1', 'BidVolume1', 'AskVolume1', 'Turnover', 'TradingDay', 'LocalTime']
+
+numeric_cols = ['Volume', 'LastPrice', 'OpenInterest', 'PreSettlementPrice',
+        'PreClosePrice', 'PreOpenInterest', 'OpenPrice', 'HighestPrice', 'LowestPrice',
+        'UpperLimitPrice', 'LowerLimitPrice', 'BidPrice1', 'AskPrice1', 'BidVolume1', 'AskVolume1', 'Turnover']
 
 def MACD(mp, window_size):
     ema_long = mp.ewm(span=2*window_size, adjust=False).mean()
@@ -57,24 +62,30 @@ class DataLoader:
         df_x = df[df['InstrumentID'] == 'IF2009']
         df_y = df[df['InstrumentID'] == 'IF2008']
         df = pd.merge(df_x, df_y, on=['time_str'], how='inner')
-        df['mid_delta'] = df['mp_x'] - df['mp_y']
+        df['mid_spread'] = df['mp_x'] - df['mp_y']
         for t in time_set:
             for s in ['x', 'y']:
                 df['mid_diff_%s_%d' % (s, t)] = df['mp_%s' % (s)].diff(t)
                 x_col.append('mid_diff_%s_%d' % (s, t))
                 df['openinterest_diff_%s_%d' % (s, t)] = df['OpenInterest_%s' % (s)].diff(t)
                 x_col.append('openinterest_diff_%s_%d' % (s, t))
-                df['mid_delta_diff_%d' % (t)] = df['mid_delta'].diff(t)
-                x_col.append('mid_delta_diff_%d' % (t))
+                df['mid_spread_diff_%d' % (t)] = df['mid_spread'].diff(t)
+                x_col.append('mid_spread_diff_%d' % (t))
                 df['wp-mp_diff_%s_%d' % (s, t)] = df['wp-mp_%s' %(s)].diff(t)
                 x_col.append('wp-mp_diff_%s_%d' % (s, t))
                 df['atp-mp_diff_%s_%d' % (s, t)] = df['atp-mp_%s' %(s)].diff(t)
                 x_col.append('atp-mp_diff_%s_%d' % (s, t))
                 df['rwp-mp_diff_%s_%d' % (s, t)] = df['rwp-mp_%s' %(s)].diff(t)
                 x_col.append('rwp-mp_diff_%s_%d' % (s, t))
+                df['mid_spread_%d' % (t)] = df['mid_spread'].shift(t)
+                x_col.append('mid_spread_%d'%(t))
             if t > 1:
-                df['mid_delta_gap_%d'%(t)] = (df['mid_delta'] - df['mid_delta'].rolling(t).mean())/(df['mid_delta'].rolling(t).std()+1e-5)
-                x_col.append('mid_delta_gap_%d'%(t))
+                df['mid_spread_gap_%d'%(t)] = (df['mid_spread'] - df['mid_spread'].rolling(t).mean())/(df['mid_spread'].rolling(t).std()+1e-5)
+                x_col.append('mid_spread_gap_%d'%(t))
+                df['mid_spread_min_diff_%d'%(t)] = (df['mid_spread'] - df['mid_spread'].rolling(t).min())
+                x_col.append('mid_spread_min_diff_%d'%(t))
+                df['mid_spread_max_diff_%d'%(t)] = -(df['mid_spread'] - df['mid_spread'].rolling(t).max())
+                x_col.append('mid_spread_max_diff_%d'%(t))
                 df['macd_mid_%s_%d' % (s, t)] = MACD(df['mp_%s'%(s)], t)
                 x_col.append('macd_mid_%s_%d' % (s, t))
                 
@@ -82,12 +93,15 @@ class DataLoader:
     
     x_col += ['wp-mp_x', 'atp-mp_x', 'rwp-mp_x']
     x_col += ['wp-mp_y', 'atp-mp_y', 'rwp-mp_y']
+
+    #x_col += [nc + s for nc in numeric_cols for s in ['_x', '_y']]
     
     x_col = list(set(x_col))
         
     df = reduce(lambda x,y: x.append(y), df_list)
     df[y_col] = df['r10_x'] - df['r10_y']
-    
+    df = df.reset_index()
+
     x, y = df[x_col].values, df[y_col].values
     x = RobustScaler().fit_transform(x)
     self.x, self.y, self.df, self.x_col, self.y_col = x, y, df, x_col, y_col
@@ -99,15 +113,53 @@ class DataLoader:
     x_train, x_valid, y_train, y_valid = train_test_split(x_train, y_train, test_size = 0.2, random_state=1)
     return x_train, x_valid, x_test, y_train, y_valid, y_test
 
-  def Plot(self):
-    self.PlotTargetDist()
-    PlotFactorCorr(self.x, self.y)
-
-  def PlotTargetDist(self):
-    plt.title('y distribution')
-    plt.hist(self.df[~np.isnan(self.df[self.y_col])][self.y_col])
-    plt.grid()
+  def Plot(self, fig_size=(15, 6), nrow = 2, ncol = 2):
+    self.fig, ax = plt.subplots(nrows=nrow, ncols=ncol, figsize=fig_size)
+    count = 0
+    self.PlotTargetMove(ax[int(count/ncol)%nrow, count%ncol])
+    count += 1
+    self.PlotTargetDist(ax[int(count/ncol)%nrow, count%ncol])
+    count += 1
+    self.corr = PlotFactorCorr(ax=ax[int(count/ncol)%nrow, count%ncol], x=self.x, y=self.y)
+    count += 1
+    self.PlotSpread(ax=ax[int(count/ncol)%nrow, count%ncol])
     plt.show()
+    self.fig, ax = plt.subplots(nrows=1, ncols=2, figsize=fig_size)
+    count = 0
+    self.PlotCorrRank(ax[0])
+    count += 1
+    self.PlotCorrHeatMap(ax[1])
+    count += 1
+    plt.show()
+
+  def PlotSpread(self, ax):
+    ax.set_title('spread move')
+    ax.plot(self.df['mid_spread'], label='mid_spread')
+    ax.grid()
+
+  def PlotCorrHeatMap(self, ax, topn=8):
+    ax.set_title('top%d corr heatmap' %(topn))
+    scol = [self.x_col[i[0]] for i in self.sorted_corr[:topn]]
+    #ax.set_xticklabels(scol)
+    #ax.set_yticklabels(scol)
+    im = ax.matshow(self.df[scol].corr())
+    self.fig.colorbar(im, ax=ax)
+
+  def PlotCorrRank(self, ax, topn=20):
+    self.sorted_corr = sorted(self.corr.items(), key=lambda x: abs(x[1]), reverse=True)
+    ax.set_title('top %d corr' %(topn))
+    ax.barh(range(topn), [t[1] for t in self.sorted_corr[:topn]], tick_label = [self.x_col[t[0]] for t in self.sorted_corr[:topn]])
+
+  def PlotTargetMove(self, ax):
+    ax.plot(self.y)
+    ax.set_title('y move')
+    ax.grid()
+    ax.legend()
+
+  def PlotTargetDist(self, ax):
+    ax.set_title('y distribution')
+    ax.hist(self.df[~np.isnan(self.df[self.y_col])][self.y_col])
+    ax.grid()
 
 if __name__ == '__main__':
   dl = DataLoader('../data/')
--- problem1/ml_util.py
+++ problem1/ml_util.py
@@ -1,1 +1,88 @@
-/root/quant/tools/ml_util.py
+import pandas as pd
+import numpy as np
+import os
+import sys
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from xgboost import plot_importance
+import itertools
+from tensorflow.keras import Model
+import xgboost
+
+def GetMidRes(model, layer_name, x):
+  mid_model = Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
+  return mid_model.predict(x)
+
+def PlotRoc(y_test, y_pred):
+  from sklearn.metrics import roc_curve, auc
+  fpr, tpr, thr = roc_curve(y_test, y_pred)
+  roc_auc = auc(fpr, tpr)
+  plt.xlabel('False Positive Rate')
+  plt.ylabel('True Positive Rate')
+  plt.title('ROC curve')
+  plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+  plt.legend()
+  plt.show()
+
+def PlotFactorCorr(x, y, topn=20, ax=None):
+  corr = {}
+  for i in range(len(x[0])):
+    c = pd.Series(x[:, i]).corr(pd.Series(y))
+    if not np.isnan(c):
+      corr[i] = (c)
+  if ax == None:
+    plt.title('corr hist between xi and y')
+    plt.hist(corr.values())
+    plt.grid()
+    plt.show()
+  else:
+    ax.set_title('corr between xi and y')
+    ax.hist(corr.values())
+    ax.grid()
+  return corr
+
+def plot_confusion_matrix(cm,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    classes = range(cm.shape[0])
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.show()
+
+def ClsReport(model, y_test, y_pred, binary=False):
+  cm = confusion_matrix(y_test, y_pred)
+  plot_confusion_matrix(cm)
+  target_names = ['class%d'%(i) for i in range(cm.shape[0])]
+  print(classification_report(y_test, y_pred, target_names=target_names))
+  if binary:
+    PlotRoc(y_test, y_pred)
+  if isinstance(model, xgboost.sklearn.XGBClassifier):
+    ax = plot_importance(model, max_num_features=15)
+    #return [yt._text for yt in ax.yticklabels]
+    return [yt.label._text for yt in ax.yaxis.majorTicks]
+  return None
